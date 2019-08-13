be556cbd148831bfb11c97cb2a47c6f09b29bbfd
Sala 310 <brahianestiven08@gmail.com>
2019-08-13 14:33:15 -0500

    up

--- src/component/Portafoliosection.jsx
+++ src/component/Portafoliosection.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import imagen1 from '../img/portfolio/thumbnails/1.jpg';
-import image from '../img/portfolio/fullsize/1.jpg';
+import imagen1 from '../img/portfolio/thumbnails/hackthon.png';
+import image from '../img/portfolio/fullsize/hackthon.png';
 import imagen2 from '../img/portfolio/thumbnails/geek.jpg';
 import geek from '../img/portfolio/fullsize/geek.jpg';
 import go from '../img/portfolio/thumbnails/recycling.jpg';
 import recycling from '../img/portfolio/fullsize/recycling.jpg';
+import tienda from '../img/portfolio/fullsize/tienda.png';
+import tienda2 from '../img/portfolio/thumbnails/tienda.png';
+
+
 const Portafoliosection=()=>{
     return(
         <div>
@@ -16,10 +20,12 @@ const Portafoliosection=()=>{
             <img className="img-fluid" src={imagen1} alt="beta"/>
              <div className="portfolio-box-caption">
               <div className="project-category text-white-50">
-                testing
+                hackathon
               </div>
             <div className="project-name">
-                beta test gamer
+            <a href="https://eager-cray-2e7348.netlify.com/" target="_blank">
+                Academia geek
+                </a>
               </div>
             </div>
           </a>
@@ -34,8 +40,22 @@ const Portafoliosection=()=>{
               <div className="project-name">
                 <a href="https://hardcore-feynman-9f00f3.netlify.com/" target="_blank">
                 Geek conferencia.
-                </a>
-                
+                </a> 
+              </div>
+            </div>
+          </a>
+        </div>
+        <div className="col-lg-4 col-sm-6">
+          <a className="portfolio-box" href={tienda}>
+            <img className="img-fluid" src={tienda2} alt=""/>
+            <div className="portfolio-box-caption">
+              <div className="project-category text-white-50">
+                Tienda online
+              </div>
+              <div className="project-name">
+                <a href="https://tienda-virtual-geek.firebaseapp.com" target="_blank">
+                Tienda online
+                </a> 
               </div>
             </div>
           </a>
@@ -54,24 +74,9 @@ const Portafoliosection=()=>{
           </a>
         </div>
       </div>
-    </div>
-    <div className="col-lg-4 col-sm-6">
-          <a className="portfolio-box" href={imagen2}>
-            <img className="img-fluid" src={geek} alt=""/>
-            <div className="portfolio-box-caption">
-              <div className="project-category text-white-50">
-                traformar a react
-              </div>
-              <div className="project-name">
-                <a href="https://hardcore-feynman-9f00f3.netlify.com/" target="_blank">
-                Geek conferencia.
-                </a>
-                
-              </div>
-            </div>
-          </a>
-        </div>
-  </section>
+    </div>   
+      </section>
+      
         </div>
     )
 }
